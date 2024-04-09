9d1dcdcb58e85ee86448689bb621c102f284774d
xayyeem <kjafri30izback@gmail.com>
2024-04-09 15:05:15 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import './App.css'
 import Navbar from './component/Navbar'
 import Hero from './component/Hero'
 import Featured from './component/Featured'
+import Workflow from './component/Workflow'
+import Pricing from './component/Pricing'
+import Testimonials from './component/Testimonials'
+import Footer from './component/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -13,6 +17,10 @@ function App() {
       <div className="max-w-7xl mx-auto pt-20 px-6">
         <Hero />
         <Featured />
+        <Workflow />
+        <Pricing />
+        <Testimonials />
+        <Footer />
       </div>
     </>
   )
--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
     return (
         <div className="flex flex-col items-center mt-6 lg:mt-20" >
             <h1 className="text-4xl sm:text-6xl lg:text-7xl text-center tracking-wide">
-                VirtualR build tools
+                VirtualX build tools
                 <span className="bg-gradient-to-r from-orange-500 to-red-800 text-transparent bg-clip-text">
                     {" "}
                     for developers
